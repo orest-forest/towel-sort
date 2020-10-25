00e84dfb7b4916cdb31f1ccbc536d08d6f04bb45
orest-forest <danilchenko.orest@gmail.com>
2020-10-25 17:13:56 +0200

    feat: cpmplete task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,31 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    module.exports = function towelSort(matrix) {
+
+        if (matrix == undefined) {
+            return []
+        }
+
+        let arr = matrix;
+        let newArr = [];
+
+        for (let i = 0; i < arr.length; i++) {
+            if (i % 2 == 0) {
+                for (let j = 0; j < arr[i].length; j++) {
+                    newArr.push(arr[i][j])
+                }
+            } else {
+                for (let j = arr[i].length - 1; j => 0; j--) {
+                    newArr.push(arr[i][j])
+                }
+            }
+        }
+
+
+        return newArr
+
+    };
+
+
 }
